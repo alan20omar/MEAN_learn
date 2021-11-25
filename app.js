70325cf2505527f12f853266710f5e142245967b
Alan <lozadasanchezao@gmail.com>
2021-11-24 20:18:11 -0600

    Implementación de metodos get, post, put, patch y delete del modelo task

--- app.js
+++ app.js
@@ -81,12 +81,12 @@ app.post('/tasklists',(req, res) => {
         });
 })
 
-// Upadate a object (update full object)
+// Update a object (update full object)
 app.put('/tasklists/:tasklistid',(req, res) => {
     TaskList.findOneAndUpdate({_id: req.params.tasklistid},{$set: req.body})
-        .then((obUpdated) => {
+        .then((obj) => {
             res.status(201);
-            res.send(obUpdated);
+            res.send(obj);
         })
         .catch((error) => {
             res.status(500);
@@ -97,9 +97,9 @@ app.put('/tasklists/:tasklistid',(req, res) => {
 // Upadate a parcial object 
 app.patch('/tasklists/:tasklistid',(req, res) => {
     TaskList.findOneAndUpdate({_id: req.params.tasklistid},{$set: req.body})
-        .then((obUpdated) => {
+        .then((obj) => {
             res.status(200);
-            res.send(obUpdated);
+            res.send(obj);
         })
         .catch((error) => {
             res.status(500);
@@ -110,9 +110,98 @@ app.patch('/tasklists/:tasklistid',(req, res) => {
 // Delete a tasklist by id
 app.delete('/tasklists/:tasklistid', (req, res) => {
     TaskList.findByIdAndDelete({ _id: req.params.tasklistid })
-        .then((obUpdated) => {
+        .then((objdeleted) => {
             res.status(202);
-            res.send(obUpdated);
+            res.send(objdeleted);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error);
+        });
+});
+
+// CRUD operation for task, a task should always belong to a tasklist
+// Get all tasks for 1 tasklist http://localhost:3000/tasklists/:tasklist/tasks
+app.get('/tasklists/:tasklistid/tasks', (req, res) => {
+    Task.find({_taskListId: req.params.tasklistid})
+        .then((tasks) => {
+            res.status(200);
+            res.send(tasks);
+        })
+        .catch((error) =>{
+            res.status(500);
+            console.log(error);
+        });
+});
+
+// Create a task inside a particular tasklist
+app.post('/tasklists/:tasklistid/tasks', (req, res) =>{
+    let taskObject = {
+        title: req.body.title,
+        _taskListId: req.params.tasklistid,
+        commpleted: req.body.commpleted 
+    };
+    Task(taskObject).save()
+        .then((data) => {
+            res.status(201);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error)
+        });
+});
+
+// Get 1 tasks for 1 tasklist http://localhost:3000/tasklists/:tasklist/tasks/:taskid
+app.get('/tasklists/:tasklistid/tasks/:taskid', (req, res) => {
+    Task.find({ _taskListId: req.params.tasklistid, _id: req.params.taskid })
+        .then((task) => {
+            res.status(200);
+            res.send(task);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error);
+        });
+});
+
+// Update 1 task for 1 tasklist http://localhost:3000/tasklists/:tasklist/tasks/:taskid
+app.put('/tasklists/:tasklistid/tasks/:taskid', (req, res) => {
+    let taskObject = {
+        title: req.body.title,
+        _taskListId: req.params.tasklistid,
+        completed: req.body.completed
+    };
+    Task.findOneAndUpdate({ _id: req.params.taskid, _taskListId: req.params.tasklistid }, {$set: req.body})
+        .then((obj) => {
+            res.status(201);
+            res.send(obj);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error);
+        });
+});
+
+// Update 1 partial (completed) task for 1 tasklist http://localhost:3000/tasklists/:tasklist/tasks/:taskid
+app.patch('/tasklists/:tasklistid/tasks/:taskid', (req, res) => {
+    Task.findOneAndUpdate({ _id: req.params.taskid, _taskListId: req.params.tasklistid }, {$set: req.body})
+        .then((obj) => {
+            res.status(200);
+            res.send(obj);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error);
+        });
+});
+
+// Delete 1 task of 1 tasklist
+app.delete('/tasklists/:tasklistid/tasks/:taskid', (req, res) => {
+    Task.findByIdAndDelete({ _id: req.params.taskid })
+        .then((objdeleted) => {
+            res.status(202);
+            res.send(objdeleted);
         })
         .catch((error) => {
             res.status(500);
